d0a7aa5b2214b26221feaed1db226f6df6d7b2ba
Evgeniya88 <Paladina.EM@kvt.su>
2020-06-27 16:05:20 +0300

    Разработана шапка сайта адаптированная под моб устройства

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
+"use strict";
 
+//Burger menu
+let burgerEl = document.querySelector('.burger');
+  
+
+function actionBurgerMenu() {
+	let menuEl = document.querySelector('.main-nav__list');
+	this.classList.toggle('main-nav__burger--active');
+	menuEl.classList.toggle('main-nav__list--active');
+}
+
+burgerEl.addEventListener("click", actionBurgerMenu);
+
+let prodNavEl = document.getElementById('main-nav__prod');
+let prodListEl = document.querySelector('.nav-item__submenu');
+let prodCloseBtn = document.querySelector('.submenu__close');
+
+function openProdList(e) {	
+	e.preventDefault();
+	prodListEl.classList.toggle('nav-item__submenu--active');
+}
+
+prodNavEl.addEventListener("click", openProdList);
+
+prodCloseBtn.addEventListener("click", function() {
+	prodListEl.classList.remove('nav-item__submenu--active');
+});
 
