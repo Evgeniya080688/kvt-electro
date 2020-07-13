c6af45959197d8b6949ee23a5aac90f519123796
Evgeniya88 <Paladina.EM@kvt.su>
2020-07-13 14:10:25 +0300

    Внесены правки в отображение меню на мобильной версии

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,16 @@ import * as banerAction from './banner.js';
 
 //Burger menu
 let burgerEl = document.querySelector('.burger');
-  
+let menuEl = document.querySelector('.main-nav__list');
+let mainMenuEl = document.querySelector('.header__menu');
+let headerEl = document.querySelector('.main-header');
 
 function actionBurgerMenu() {
-	let menuEl = document.querySelector('.main-nav__list');
 	this.classList.toggle('main-nav__burger--active');
 	menuEl.classList.toggle('main-nav__list--active');
+	if (!(this.classList.contains('main-nav__burger--active'))) {
+		prodListEl.classList.remove('nav-item__submenu--active');
+	}
 }
 
 burgerEl.addEventListener("click", actionBurgerMenu);
@@ -20,6 +24,7 @@ let prodCloseBtn = document.querySelector('.submenu__close');
 
 function openProdList(e) {	
 	e.preventDefault();
+	document.documentElement.scrollTop = 0;
 	prodListEl.classList.toggle('nav-item__submenu--active');
 }
 
@@ -28,6 +33,21 @@ prodNavEl.addEventListener("click", openProdList);
 prodCloseBtn.addEventListener("click", function() {
 	prodListEl.classList.remove('nav-item__submenu--active');
 });
+
+
+window.addEventListener('scroll', function() {
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	if ((scrollTop > headerEl.offsetHeight) &&
+		!(prodListEl.classList.contains('nav-item__submenu--active')) && 
+		!(burgerEl.classList.contains('main-nav__burger--active'))	) {
+			mainMenuEl.classList.add('fixed');
+	}
+	else {
+		mainMenuEl.classList.remove('fixed');
+	}
+	//alert( "Текущая прокрутка: " + scrollTop );
+	
+});	
 
 
 
